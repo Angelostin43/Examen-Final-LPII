1ae08b143bcb0c66868619c509a937df32d349da
Angelo Llanos <angeloztin@gmail.com>
2024-10-24 04:07:23 -0500

    Comienzo Examen

---
+++ src/main/java/com/ciberbank/ExamenFinalLpiiApplication.java
@@ -0,0 +1,13 @@
+package com.ciberbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalLpiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFinalLpiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciberbank/ExamenFinalLpiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciberbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalLpiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
